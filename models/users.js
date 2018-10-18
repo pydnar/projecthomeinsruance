4bfebd3b97d44f5f361108be6974315ca5a50308
Chad Davis <ccdavis2k@gmail.com>
2018-10-18 18:15:03 -0400

    added CRUD to users.js

--- models/users.js
+++ models/users.js
@@ -6,17 +6,26 @@ var users = {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  // create: function(cols, vals, cb) {
-  //   orm.create("users", cols, vals, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // update: function(objColVals, condition, cb) {
-  //   orm.update("users", objColVals, condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
-};
+  create: function(cols, vals, cb) {
+    orm.create("users", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("users", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("users", condition, function(res) {
+      cb(res);
+    });
+  },
+  selectWhere: function(useremail, cb) {
+    orm.selectWhere("homeassets","id_email",useremail, function(res){
+      cb(res);
+    });
+  },
+ };
 
 module.exports = users;
