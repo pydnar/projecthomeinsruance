b807f84ed99e0982594f3eef3f39e8595ab74683
Simmigon <simmigon.flagg@gmail.com>
2018-10-18 16:21:46 -0400

    Adding update

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "hjysvfuheirkkde1",
+    password: "wrmohcvy9hb0mau7",
+    database: "c9tbk1oei4eajy1s"
+  });
+}
+
+connection.connect();
+
+module.exports = connection;
+
+//  connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// Export connection for our ORM to use.
---
+++ controllers/insurance.js
@@ -0,0 +1,49 @@
+var express = require("express");
+
+var router = express.Router();
+
+var insurance = require("../models/insurance.js");
+
+router.get("/", function(req, res) {
+  insurance.all(function(data) {
+    var insuranceObject = {
+      users: data
+    };
+    console.log(data)
+    //console.log(insuranceObject);
+    res.render("index", insuranceObject);
+  });
+});
+
+router.post("/api/insurance", function(req, res) {
+ console.log(` What are thoses ${req.body}`);
+//   burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    // Send back the ID of the new quote
+   
+//   });
+ res.json({ id: result.insertId });
+});
+
+// router.put("/api/insurance/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   insurance.update(
+//     {
+//       devoured: req.body
+//     },
+//     condition,
+//     function(result) {
+//       if (result.changedRows === 0) {
+//         // User Doesn't exist in database
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+
+//     }
+//   );
+// });
+
+// Export routes for server.js to use.
+module.exports = router;
