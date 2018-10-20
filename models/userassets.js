b9e22720312ab621f22b8364231ac0ac17bf26c4
Simmigon <simmigon.flagg@gmail.com>
2018-10-20 17:44:25 -0400

    Users and assets are together awwwwww

---
+++ models/userassets.js
@@ -0,0 +1,31 @@
+// Data 
+var orm = require("../config/orm.js");
+
+var users = {
+  all: function (cb) {
+    orm.all("users", function (res) {
+      cb(res);
+    });
+  },
+}
+
+var homeassets = {
+  all: function (cb) {
+    orm.all("homeassets", function (res) {
+      cb(res);
+    });
+  }
+  // The variables cols and vals are arrays.
+  // create: function(cols, vals, cb) {
+  //   orm.create("insurance", cols, vals, function(res) {
+  //     cb(res);
+  //   });
+  // },
+  // update: function(objColVals, condition, cb) {
+  //   orm.update("insurance", objColVals, condition, function(res) {
+  //     cb(res);
+  //   });
+  // }
+};
+
+module.exports = { users, homeassets };
