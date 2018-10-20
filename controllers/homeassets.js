01f9ed69b77ea3ccd115d0f15a879ea1fdc4a983
Simmigon <simmigon.flagg@gmail.com>
2018-10-20 12:40:00 -0400

    If applied you can now search the api data by email. Example  localhost:5000/api/homeassets/admin3@admin.com

--- controllers/homeassets.js
+++ controllers/homeassets.js
@@ -3,13 +3,39 @@ var express = require("express");
 var router = express.Router();
 
 var assets = require("../models/homeassets.js");
+// '51', 'Wine - Mondavi Coastal Private', '25.15', 'kaldam1@biglobe.ne.jp', '93.20', '95', '59.49', '66.77', 'http://dummyimage.com/250x216.bmp/5fa2dd/ffffff'
 
 router.get("/api/homeassets", function(req, res) {
   assets.all(function(data) {
     var assetsObject = {
       homeassets: data
     };
+    var asset = assetsObject["homeassets"];
+
+    for (var i = 0; i < asset.length; i++) {
+      if (asset[i]["id_email"] == "kaldam1@biglobe.ne.jp") {
+        console.log(asset[i]);
+      }
+    }
     res.json(assetsObject);
+  });
+});
+
+router.get("/api/homeassets/:email", function(req, res) {
+  var email = req.params.email;
+  assets.all(function(data) {
+    var assetsObject = {
+      homeassets: data
+    };
+    var asset = assetsObject["homeassets"];
+    var arrayofObject = [];
+    for (var i = 0; i < asset.length; i++) {
+      if (asset[i]["id_email"] == email) {
+        arrayofObject.push(asset[i]);
+        console.log(asset[i]);
+      }
+    }
+    res.json(arrayofObject);
   });
 });
 
